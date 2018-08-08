5581c9ab8420d4e29864d70f466047d64fe0b62a
ahmed <ahmedgh@rosafian.com>
2018-08-08 22:39:51 +0100

    position relative aboslute + IIFE

---
+++ positionCSS/script.js
@@ -0,0 +1,21 @@
+(function () {
+    ({
+        'plugins': ['jsdom-quokka-plugin'],
+        'jsdom': {
+            'file': 'DomManipule/index.html'
+        }
+    })
+
+
+    const btn = document.querySelectorAll('.btn');
+
+    btn.forEach(res => {
+        res.addEventListener('click', () => {
+            if(document.querySelector('.toolClick')){
+                document.querySelector('.toolClick').remove();
+            }
+            res.querySelector('.tool').classList.add('toolClick');
+        }, false)
+    })
+
+})()
